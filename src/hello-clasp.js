d9f5acddf5ae84664412fc7538b65ba5b3406657
Tailor VJ <tailor.vj@gmail.com>
2023-04-16 14:18:09 +0200

    hello clasp

---
+++ src/hello-clasp.js
@@ -0,0 +1,11 @@
+function helloWorld() {
+  Logger.log('Hello, World!');
+  var ui = SlidesApp.getUi();
+  ui.alert('Hello, World!');
+}
+
+function onOpen() {
+  var ui = SlidesApp.getUi();
+  ui.createMenu('Hello, World!').addItem('Say Hello', 'helloWorld').addToUi();
+}
+
